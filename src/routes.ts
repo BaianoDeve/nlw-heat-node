7e85f5ca47738a2f68d1ac27911e292293c21344
Baianinho <coelhoeliteyt@gmail.com>
2021-10-19 19:01:38 -0300

    Feat: Profile User

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { ensureAuthenticated } from '@middlewares/ensureAuthenticated';
 import { AuthenticateUserController } from '@controllers/AuthenticateUserController';
 import { CreateMessageController } from '@controllers/CreateMessageController';
 import { GetLast3MessagesController } from '@controllers/GetLast3MessagesController';
+import { ProfileUserController } from '@/controllers/ProfileUserController';
 
 const router = Router();
 
@@ -15,6 +16,8 @@ router.post(
 );
 
 router.get('/messages/last3', new GetLast3MessagesController().handle);
+
+router.get('/me', ensureAuthenticated, new ProfileUserController().handle);
 
 router.get('/github', (request: Request, response: Response) => {
   response.redirect(
